40032cffdf67941dfcaee31f7d1eaa751980e353
ayalaka <7178837@gmail.com>
2018-07-27 13:24:32 +0300

    add controllers file

---
+++ controllers/auth.controller.js
@@ -0,0 +1,23 @@
+﻿
+var express = require('express');
+var app = express();
+var router = express.Router();
+
+var mongoose = require('mongoose'); //get DB
+var connection = mongoose.createConnection('mongodb://ayalaka:artopico1234@ds040898.mlab.com:40898/artop');//connect to the db server
+var patientSchema = require('../models/patient');
+var patient = connection.model('patient', patientSchema);
+var projectSchema = require('../models/project');
+var project = connection.model('project', projectSchema);
+
+
+//var request = require('request');
+
+// routes
+router.post('/register', function (req, res) {
+    
+});
+
+module.exports = router;
+
+
